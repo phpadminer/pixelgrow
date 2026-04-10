0c0db0e5d9557426f1ed15b7d440fbe600611e1c
phpadminer <wlt157459@163.com>
2026-04-10 11:15:18 +0800

    fix: enable global ValidationPipe with transform

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  await app.listen(3000)
-  console.log('[PixelGrow Server] Running on http://localhost:3000')
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+  const port = process.env.PORT || 3001
+  await app.listen(port)
+  console.log(`[PixelGrow Server] Running on http://localhost:${port}`)
 }
 bootstrap()
